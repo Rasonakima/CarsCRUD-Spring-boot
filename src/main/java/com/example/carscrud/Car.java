9ddb5cbcbcb4f292a780aca755aa122680623171
Rasonakima <Rasonakima@gmail.com>
2019-06-18 13:17:44 +0000

    Added Car entity and repository

---
+++ src/main/java/com/example/carscrud/Car.java
@@ -0,0 +1,39 @@
+package com.example.carscrud;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.lang.NonNull;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private @NonNull String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Car() {
+        super();
+    }
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
