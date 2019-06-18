4a43fab20d487aa64a6b52bd60d05ac8279ebe3c
Rasonakima <Rasonakima@gmail.com>
2019-06-18 13:27:00 +0000

    Added toString to Car

--- src/main/java/com/example/carscrud/Car.java
+++ src/main/java/com/example/carscrud/Car.java
@@ -36,4 +36,9 @@ public class Car {
     public Car(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Car [id=" + id + ", name=" + name + "]";
+    }
 }
